ca7c1c93b76f9ad6a701053ab84fe1088de37ec1
Nahum <losond8@gmail.com>
2021-03-19 09:26:39 -0400

    Passing a prop to a component, using conditional rendering, defiing a class component, using props, constructor.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,48 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            secondsLeft: this.props.initialCount
+        }
+
+    }
+    
+    render() {
+        let button;
+        if (this.state.secondsLeft === 0) {
+           button = <div>Boom!</div>
+        } else {
+            button = <div>120 seconds left before I go boom!</div>
+        }
+    
+        return (
+          <div>
+            {button}
+          </div>
+        )
+    }
+
+}
+
+export default Bomb;
+
+
+
+// let firstVal = "Boom!"
+// let secVal = `{this.secondsLeft} seconds left before I go boom!`
+      
+// let test = () =>{
+//     if(this.secondsLeft = 0){
+//             firstVal = "Boom!"
+//         }
+//         else {
+//             secVal
+//         }
+// }
+// test()
+//           return (
+//             <div>{test}</div>
+//           );
